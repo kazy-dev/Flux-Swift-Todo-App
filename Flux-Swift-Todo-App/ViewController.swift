9d9d7ac7cf45ead1e306a75b46762a52c61a642c
kazy <kazy.dev@gmail.com>
2019-10-28 17:19:07 +0900

    add Project.

---
+++ Flux-Swift-Todo-App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Flux-Swift-Todo-App
+//
+//  Copyright © 2019 kazy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Flux-Swift-Todo-AppTests/Flux_Swift_Todo_AppTests.swift
@@ -0,0 +1,33 @@
+//
+//  Flux_Swift_Todo_AppTests.swift
+//  Flux-Swift-Todo-App
+//
+//  Copyright © 2019 kazy. All rights reserved.
+//
+
+import XCTest
+@testable import Flux_Swift_Todo_App
+
+class Flux_Swift_Todo_AppTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
